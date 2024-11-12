63fb100d199701631b824a081301f3f9e0ad82d6
yesihere <jklmn9139@gmail.com>
2024-11-12 21:43:37 +0700

    upload tugas

---
+++ MVC/controllers/about.php
@@ -0,0 +1,22 @@
+<?php 
+
+class about extends controller {
+    public function index($nama = 'Yesi', $pekerjaan = 'Mahasiswa', $umur = 18)
+    {
+        $data['nama'] = $nama;
+        $data['pekerjaan'] = $pekerjaan;
+        $data['umur'] = $umur;
+        $data['judul'] = 'About Me';
+        $this->view('templates/header', $data);
+        $this->view('about/index', $data);
+        $this->view('templates/footer');
+    }
+
+    public function page()
+    {
+        $data['judul'] = 'Pages';
+        $this->view('templates/header', $data);
+        $this->view('about/page');
+        $this->view('templates/footer');
+    }
+}
---
+++ MVC/public/index.php
@@ -0,0 +1,5 @@
+<?php 
+
+require_once '../App/init.php';
+
+$app = new App;
